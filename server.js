6fad0c1b617cc49fd9a831462f98384dd79b36d8
bjwine0 <bradsmilek@gmail.com>
2019-05-09 19:21:11 -0700

    error handler done?

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 // API routes -- render the search form
 app.get('/', newSearch);
-// app.get('/', createSearch);
+// app.get('/', handleError);
 
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
@@ -34,18 +34,19 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 //Helper functions
 
 function Book(info) {
-  // this.placeholderImage = `https://i.imgur.com/J5LVHEL.jpg`;
   this.title = info.volumeInfo.title || 'No title available';
   this.authors = info.volumeInfo.authors || 'No author available';
-  this.publishedDate = info.volumeInfo.publishedDate || 'No date of publication available';
+  this.description = info.volumeInfo.description || 'No description available';
   this.imageLinks = `http://books.google.com/books/content?id=${info.id}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api`;
 
 }
 
-// EJS file extension is required
+// EJS file extension is not required
 function newSearch (request, response){
   response.render('pages/index');
 }
+
+
 
 // NO API key required
 // you should console.log(request.body) AND (request.body.search); all that.
@@ -54,25 +55,22 @@ function createSearch(request, response){
   let url ='https://www.googleapis.com/books/v1/volumes?q=';
   if (request.body.search[1]==='title') {url+= `+intitle:${request.body.search[0]}`;}
   if (request.body.search[1]==='author') {url+= `+inauthor:${request.body.search[0]}`;}
-
-  console.log(url);
-
+  // console.log(url);
   // response.send('Ok');
 
   superagent.get(url)
-    
+
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
-    
-    .then(results => response.render('pages/searches/show', {searchResults: results}));
+    .then(results => response.render('pages/searches/show', {searchResults: results}))
 
 
 
-  // .catch(err=>handleError(err,response));
+    .catch(err => handleError(err,response));
 
 }
 
-// function handleError(err, response){
-//   console.log(err);
-//   if(response)response.status(500).send('Sorry, something went wrong');
-
-// }
+function handleError(err, res){
+  console.log('err',err);
+  console.log('response+++++++++++++++++++++++++++++++++', res);
+  if (res) res.render('pages/error');
+}
